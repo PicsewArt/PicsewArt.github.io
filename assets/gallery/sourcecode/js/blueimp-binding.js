42f8175121b4dd483ba67fe4f345af0e94b75a6d
Señorita <Senhorita@163.com>
2020-05-17 09:03:17 +0800

    -

--- assets/gallery/sourcecode/js/blueimp-binding.js
+++ assets/gallery/sourcecode/js/blueimp-binding.js
@@ -2,6 +2,7 @@ function bindGalleryEventByElement(element) {
   element.onclick = function (event) {
     event = event || window.event
     var target = event.target || event.srcElement
+    console.dir(target);
     var link = target.src ? (target.parentNode.href ? target.parentNode : target.parentNode.parentNode) : (target.href ? target : target.parentNode);
     // // this will be very slow:
     // var link = target.src
