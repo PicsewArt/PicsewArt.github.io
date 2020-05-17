4f707017a12c0d3973e26b07d96f9d88df1642b1
Señorita <Senhorita@163.com>
2020-05-17 17:57:16 +0800

    -

--- assets/gallery/sourcecode/js/blueimp-binding.js
+++ assets/gallery/sourcecode/js/blueimp-binding.js
@@ -35,4 +35,4 @@ function bindGalleryEventByClassName(elementClassName) {
     bindGalleryEventByElement(item);
   });
 }
-// document.getElementById("grouped-list-section-s").onclick=function(event){event=event||window.event;var target=event.target||event.srcElement,link=target.src?target.parentNode:target,options={index:link,event:event},links=this.getElementsByTagName("a");blueimp.Gallery(links,options)};
+// document.getElementById("grid-list-section-s").onclick=function(event){event=event||window.event;var target=event.target||event.srcElement,link=target.src?target.parentNode:target,options={index:link,event:event},links=this.getElementsByTagName("a");blueimp.Gallery(links,options)};
