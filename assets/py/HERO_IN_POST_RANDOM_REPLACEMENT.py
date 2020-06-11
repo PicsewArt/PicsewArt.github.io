92e0166b7becab952d1645f05e05725356838665
Picsew <Picsew@126.com>
2020-06-12 02:55:58 +0800

    py

---
+++ assets/py/HERO_IN_POST_RANDOM_REPLACEMENT.py
@@ -0,0 +1,27 @@
+import re
+from os import listdir
+from os.path import isfile, join
+import fileinput
+import random
+
+basepath = '/Volumes/Shared/Common/Git/Picsew/_posts/'
+hero_count = 59
+
+onlyfiles = [f for f in listdir(basepath) if isfile(join(basepath, f))]
+
+r = re.compile("  url: '.+?\.jpg'", re.MULTILINE | re.DOTALL)
+
+for afile in onlyfiles:
+	filepath = basepath + afile
+	f1 = open(filepath, 'r')
+	filedata = f1.read()
+	f1.close()
+	first_search = r.search(filedata)
+	if first_search:
+		num = random.randint(1, hero_count)
+		hero = "  url: 'HERO_00%02d.jpg'" % num
+		newdata = filedata.replace(first_search.group(), hero)
+		f2 = open(filepath, 'w')
+		f2.write(newdata)
+		f2.close()
+		print(hero)
