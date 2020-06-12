09dc52b771e588625b4566ec021c93622daf2444
Picsew <Picsew@126.com>
2020-06-13 00:40:59 +0800

    home

---
+++ home/script.js
@@ -0,0 +1,64 @@
+/* 
+=======================================
+Mobile menu functionality
+=======================================
+*/
+
+$(".open-burger").on("click", function() {
+		$("#tattoo-Nav").css("width", "100%");
+	});
+
+$(".close-burger").on("click", function() {
+		$("#tattoo-Nav").css("width", "0%");
+	});
+
+
+
+
+/* 
+=======================================
+Scroll to anchor id's
+=======================================
+*/
+
+// Select all links with hashes
+
+$('a[href*="#"]')
+	// Remove links that don't actually link to anything
+	.not('[href="#"]')
+	.not('[href="#0"]')
+	.click(function(event) {
+		// On-page links
+		if (
+			location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") &&
+			location.hostname == this.hostname
+		) {
+			// Figure out element to scroll to
+			var target = $(this.hash);
+			target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+			// Does a scroll target exist?
+			if (target.length) {
+				// Only prevent default if animation is actually gonna happen
+				event.preventDefault();
+				$("html, body").animate(
+					{
+						scrollTop: target.offset().top
+					},
+					1000,
+					function() {
+						// Callback after animation
+						// Must change focus!
+						var $target = $(target);
+						$target.focus();
+						if ($target.is(":focus")) {
+							// Checking if the target was focused
+							return false;
+						} else {
+							$target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+							$target.focus(); // Set focus again
+						}
+					}
+				);
+			}
+		}
+	});
